d620ada59860a2ac2cf925bc3f22109b59ecb6ac
caeisinger <55706339+caeisinger@users.noreply.github.com>
2019-10-31 10:39:21 -0400

    Update makeDB.sql
    deceased_horses and living_horses tables added

--- makeDB.sql
+++ makeDB.sql
@@ -1,1 +1,4 @@
 
+create table deceased_horses(name VARCHAR(50), breed VARCHAR(30), gender VARCHAR(30), dob DATE, UKYCase INT, RRCase INT PRIMARY KEY, trained VARCHAR(30), raceOutsideUSA VARCHAR(30), ageFirstRace DOUBLE);
+
+create table living_horses(name VARCHAR(50), breed VARCHAR(30), gender VARCHAR(30), dob DATE, UKYCase INT PRIMARY KEY, trained VARCHAR(30), raceOutsideUSA VARCHAR(30), ageFirstRace DOUBLE);
