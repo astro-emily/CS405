bbbfb1c2e6d25550908088a6576e31aea6753f64
caeisinger <55706339+caeisinger@users.noreply.github.com>
2019-12-07 20:48:56 -0500

    Update living_data.php

--- living_data.php
+++ living_data.php
@@ -6,7 +6,7 @@ if (!connect) { die('Connection Failed: ' . mysql_error());
 
 { mysql_select_db(“database_name”, $connect);
 
-$user_info = “INSERT INTO deceased_horses(name, breed, gender, dob, UKYCase, trained, raceOutsideUSA, ageFirstRace ) VALUES ('$_POST[name]', '$_POST[breed]', '$_POST[gender]', '$_POST[UKYCase]', '$_POST[trained]', '$_POST[raceOutsideUSA]', '$_POST[ageFirstRace]')”;
+$user_info = “INSERT INTO living_horses(name, breed, gender, dob, UKYCase, trained, raceOutsideUSA, ageFirstRace ) VALUES ('$_POST[name]', '$_POST[breed]', '$_POST[gender]', '$_POST[UKYCase]', '$_POST[trained]', '$_POST[raceOutsideUSA]', '$_POST[ageFirstRace]')”;
 
 if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
 
