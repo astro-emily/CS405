dd54948d4120dca26a85e7b92211771c7ab7d2d4
caeisinger <55706339+caeisinger@users.noreply.github.com>
2019-12-07 20:58:59 -0500

    Update hindlimb_data.php

--- hindlimb_data.php
+++ hindlimb_data.php
@@ -1,1 +1,15 @@
+<?php
 
+$connect = mysql_connect("localhost", "root", "SpotSid101929!", "equine");
+
+if (!connect) { die('Connection Failed: ' . mysql_error());
+
+{ mysql_select_db(“database_name”, $connect);
+
+$user_info = “INSERT INTO deceased_horses(DOB, name) VALUES ('$_POST[DOB]', '$_POST[name]')”;
+
+if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
+
+echo “Yeehaw! Your information was added, pardner.”;
+
+mysql_close($connect); ?>
