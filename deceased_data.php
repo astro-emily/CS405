84da8f83cc2ac1dc0aff34a9dffac3fbef09b3d6
caeisinger <55706339+caeisinger@users.noreply.github.com>
2019-12-07 20:36:02 -0500

    Update deceased_data.php

---
+++ deceased_data.php
@@ -0,0 +1,15 @@
+<?php
+
+$connect = mysql_connect("localhost", "root", "SpotSid101929!", "equine");
+
+if (!connect) { die('Connection Failed: ' . mysql_error());
+
+{ mysql_select_db(“database_name”, $connect);
+
+$user_info = “INSERT INTO deceased_horses(name, breed, gender, dob, UKYCase, RRCase, trained, raceOutsideUSA, ageFirstRace ) VALUES ('$_POST[name]', '$_POST[breed]', '$_POST[gender]', '$_POST[UKYCase]', '$_POST[RRCase]', '$_POST[trained]', '$_POST[raceOutsideUSA]', '$_POST[ageFirstRace]')”;
+
+if (!mysql_query($user_info, $connect)) { die('Error: ' . mysql_error()); }
+
+echo “Yeehaw! Your information was added, pardner.”;
+
+mysql_close($connect); ?>
