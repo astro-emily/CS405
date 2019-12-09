933388dfda0c8738cb82459033d17d88f85c4bba
Emily Essex <emilygrace.essex@gmail.com>
2019-12-08 23:25:29 -0500

    fixed formatting

--- profile.php
+++ profile.php
@@ -13,14 +13,17 @@ header("location: index.php"); // Redirecting To Home Page
 </head>
 <body>
 <div id="profile">
-<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+<b id="welcome">Howdy! <i><?php echo $login_session; ?></i></b>
 <b id="logout"><a href="logout.php">Log Out</a></b>
 </div>
 <div id="data_entry_selection">
 <h2>Start a New Data Entry</h2>
-<b id="new_form"><a href="deceased_form.html">Deceased Horse Form</a></b>
-<b id="new_form"><a href="living_form.html">Living Horse Form</a></b>
-<b id="new_form"><a href="form.html">Start New</a></b>
+<ul>
+   <li id="new_form"><a href="deceased_form.html">Deceased Horse Form</a></li>
+   <li id="new_form"><a href="living_form.html">Living Horse Form</a></li>
+   <li id="new_form"><a href="forelimb_form.html">Forelimb Form</a></li>
+   <li id="new_form"><a href="hindlimb_form.html">Hindlimb Form</a></li>
+</ul>
 </div>
 </body>
 </html>
